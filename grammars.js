1252c82d4d144f5127bf344a9dfc134d1121baf1
Tonortiz16 <tonortiz16@gmail.com>
2024-09-13 20:01:25 -0600

    Add files via upload

---
+++ grammars.js
@@ -0,0 +1,15 @@
+
+
+var grammars = {
+
+
+    e : { "origin":["#colores# #adjetivos#"],
+    "colores": ["rojo","verde","verde olivo","azul","amarillo","naranja","gris"],
+"adjetivos": ["furioso","tonto","amoroso","sensible","tierno","suavecito","victimista"]
+
+
+
+
+}
+
+}
